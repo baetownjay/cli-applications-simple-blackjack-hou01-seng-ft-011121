c0d599012dee3c2b57b35027690c116dc385eb52
Jason Herrera <baytownjason@yahoo.com>
2021-01-11 06:23:38 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -68,8 +68,12 @@ end
 def runner
   # code runner here
   welcome
-  initial_round
-  hit?(deal_card)
-  
+  card_total = initial_round
+  while card_total < 21 do
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
+  
     
